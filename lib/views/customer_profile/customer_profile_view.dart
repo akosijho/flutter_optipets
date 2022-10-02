003c04c236ef8334de97c3cc55ddefb3a7465499
akosijho <jhobertpanerio@gmail.com>
2022-10-02 08:52:15 +0800

    Add circular progress indicator.

--- lib/views/customer_profile/customer_profile_view.dart
+++ lib/views/customer_profile/customer_profile_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_optipets/views/widgets/bottom_appbar.dart';
+import 'package:flutter_optipets/views/widgets/my_circular_progress.dart';
 import 'package:flutter_optipets/views/widgets/top_appbar.dart';
 
 class CustomerProfileView extends StatelessWidget{
@@ -10,6 +11,9 @@ class CustomerProfileView extends StatelessWidget{
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: myAppBar("Me"),
+      body: Center(
+        child: myCircularProgress(),
+      ),
       bottomNavigationBar: const MyBottomAppBar(
         isProfile: true,
       ),
