913ffdc8ab88d98bfb78057256bed1752e4a3c1c
akosijho <jhobertpanerio@gmail.com>
2022-10-04 11:00:27 +0800

    Refine login screen.

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -18,124 +18,131 @@ class LoginView extends StatelessWidget {
           return Scaffold(
             extendBodyBehindAppBar: true,
             backgroundColor: MyColors.blue1,
-            body: Center(
+            body: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
               child: SingleChildScrollView(
                 scrollDirection: Axis.vertical,
-                physics: const NeverScrollableScrollPhysics(),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SvgPicture.asset(SvgImages.logo, width: 196, height: 183),
-                    const SizedBox(height: 4),
-                    Text(
-                      appName.toLowerCase(),
-                      style: Theme.of(context).textTheme.headline3,
-                    ),
-                    const SizedBox(
-                      height: 66,
-                    ),
-                    const Text(
-                      "Sign in with your given account",
-                      style: TextStyle(
-                        color: MyColors.scaffoldBg,
-                        fontSize: 16,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 54),
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            controller: viewModel.usernameFieldController,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              contentPadding: Theme.of(context)
-                                  .inputDecorationTheme
-                                  .contentPadding,
-                              label: const Text("Username"),
-                              fillColor: Theme.of(context)
-                                  .inputDecorationTheme
-                                  .fillColor,
-                              filled: true,
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: const BorderSide(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 40.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SvgPicture.asset(SvgImages.logo, width: 196, height: 183),
+                        Text(
+                          appName.toLowerCase(),
+                          style: Theme.of(context).textTheme.headline3,
+                        ),
+                        const SizedBox(
+                          height: 54,
+                        ),
+                        const Text(
+                          "Sign in with your given account",
+                          style: TextStyle(
+                            color: MyColors.scaffoldBg,
+                            fontSize: 12,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 32),
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                controller: viewModel.usernameFieldController,
+                                decoration: InputDecoration(
+                                  isDense: true,
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  contentPadding: Theme.of(context)
+                                      .inputDecorationTheme
+                                      .contentPadding,
+                                  label: const Text("Username"),
+                                  fillColor: Theme.of(context)
+                                      .inputDecorationTheme
+                                      .fillColor,
+                                  filled: true,
+                                  floatingLabelBehavior:
+                                      FloatingLabelBehavior.never,
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: const BorderSide(
+                                      color: Color(0xff52575D),
+                                    ),
+                                  ),
+                                ),
+                                style: const TextStyle(
+                                  fontSize: 16,
                                   color: Color(0xff52575D),
                                 ),
                               ),
-                            ),
-                            style: const TextStyle(
-                              fontSize: 24,
-                              color: Color(0xff52575D),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 16,
-                          ),
-                          TextFormField(
-                            controller: viewModel.passwordFieldController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
+                              const SizedBox(
+                                height: 16,
                               ),
-                              contentPadding: Theme.of(context)
-                                  .inputDecorationTheme
-                                  .contentPadding,
-                              label: const Text("Password"),
-                              fillColor: Theme.of(context)
-                                  .inputDecorationTheme
-                                  .fillColor,
-                              filled: true,
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: const BorderSide(
+                              TextFormField(
+                                controller: viewModel.passwordFieldController,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  isDense: true,
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  contentPadding: Theme.of(context)
+                                      .inputDecorationTheme
+                                      .contentPadding,
+                                  label: const Text("Password"),
+                                  fillColor: Theme.of(context)
+                                      .inputDecorationTheme
+                                      .fillColor,
+                                  filled: true,
+                                  floatingLabelBehavior:
+                                      FloatingLabelBehavior.never,
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: const BorderSide(
+                                      color: Color(0xff52575D),
+                                    ),
+                                  ),
+                                ),
+                                style: const TextStyle(
+                                  fontSize: 16,
                                   color: Color(0xff52575D),
                                 ),
                               ),
-                            ),
-                            style: const TextStyle(
-                              fontSize: 24,
-                              color: Color(0xff52575D),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 48,
-                          ),
-                          SizedBox(
-                            width: double.infinity,
-                            height: 54,
-                            child: ElevatedButton(
-                              style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                  const Color(0xff00BBF0),
+                              const SizedBox(
+                                height: 48,
+                              ),
+                              SizedBox(
+                                width: double.infinity,
+                                height: 40,
+                                child: ElevatedButton(
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                      const Color(0xff00BBF0),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    viewModel.login();
+                                  },
+                                  child: const Text(
+                                    "Login",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
                                 ),
                               ),
-                              onPressed: () {
-                                viewModel.login();
-                              },
-                              child: const Text(
-                                "Login",
-                                style: TextStyle(
-                                  fontSize: 32,
-                                ),
-                              ),
-                            ),
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
